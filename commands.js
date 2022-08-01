5193bd35f227101fca8bbe199e6bc565bd64a2eb
Kingsuhas <OG.suhas@gmail.com>
2022-08-01 23:02:02 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,9 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	data: [new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'), new SlashCommandBuilder().setName('re').setDescription('Rerolls a Giveaway')],
-	
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+	data: [
+		new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'), 
+		new SlashCommandBuilder().setName('re').setDescription('Rerolls a Giveaway')
+	],
 };
